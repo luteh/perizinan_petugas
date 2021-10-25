e315c7a13317cc485a19f4cd08e9f04f1b3ca2bf
Luthfan Maftuh <luthfanmaftuh@gmail.com>
2021-10-25 19:03:07 +0700

    Change google api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAR0FTH4IqrSaw_LX43KtxTIfVSTMqsthQ")
+    GMSServices.provideAPIKey("AIzaSyB97jSWXkpNIVjUIVEkQJmJfqzAnZmPjuA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
