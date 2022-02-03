5dc416d8c56714d7e49b642cbccfe3a57f11d753
Luthfan Maftuh <luthfanmaftuh@gmail.com>
2022-02-03 16:55:41 +0700

    Change profile endpoint

--- lib/data/remote/api/endpoint.dart
+++ lib/data/remote/api/endpoint.dart
@@ -6,6 +6,6 @@ class Endpoint {
   static const requestToken = '/accounts/token/request';
   static const verificationCode = '/accounts/forgot-password/verification-code';
   static const setNewPassword = '/accounts/forgot-password/password';
-  static const getProfile = '/accounts/profile';
+  static const getProfile = '/admins/accounts/profile';
   static const updatePassword = '/accounts/profile/password';
 }
